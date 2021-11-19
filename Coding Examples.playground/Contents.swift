02538cc951c88dd943a51352abb5c142122823a3
ahmed.samir <ahmed.samir@ibtikar.net.sa>
2021-11-19 19:12:53 +0200

    Enhancement

--- Coding Examples.playground/Contents.swift
+++ Coding Examples.playground/Contents.swift
@@ -255,3 +255,58 @@ func createDictionaryFromArrayOfTuble(arrayOfTuble: [(String,String)]) -> [Strin
 }
 print(createDictionaryFromArrayOfTuble(arrayOfTuble: [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////andela
+//var prereqs_courses = [
+//    ("Foundations of Computer Science", "Operating Systems"),
+//    ("Data Structures", "Algorithms"),
+//    ("Computer Networks", "Computer Architecture"),
+//    ("Algorithms", "Foundations of Computer Science"),
+//    ("Computer Architecture", "Data Structures"),
+//    ("Software Design", "Computer Networks")
+//]
+//
+//let dictionary = prereqs_courses.reduce(into: [:]) { $0[$1.0] = $1.1 }
+//print(dictionary)
+//
+//
+//
+////Software Design
+//
+////Computer Networks
+////Computer Architecture
+//
+////Data Structures
+////Algorithms
+////Foundations of Computer Science
+//
+////Operating Systems
